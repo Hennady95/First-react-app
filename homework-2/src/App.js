b9f78598f983498a631ce8629952e0efc29c7e79
Hennady95 <gena.trusevich@mail.ru>
2021-02-20 12:10:17 +0300

    feat: complete task 2

--- homework-2/src/App.js
+++ homework-2/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
         <Square label ='Big squere' cssClass = 'square-big' />
         <Square label ='A less squere' cssClass = 'square-less' />
         <Square label ='The smallest one' cssClass = 'square-smollest' />
+        <Textlorem />
       </div>
     )
   }
@@ -21,6 +22,23 @@ class Square extends React.Component {
       <div className = {this.props.cssClass}>
         {this.props.label}
       </div>
+    )
+  }
+}
+
+class Textlorem extends React.Component {
+
+  state = {
+    italic : false
+  } 
+
+  changeStyle = () => this.state.italic ? this.setState({italic: false}) : this.setState({italic: true})
+
+  render () {
+    return (
+      <p onClick = {this.changeStyle} className = {this.state.italic === true ? 'italic-text' : ''}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
+      and scrambled it to make a type specimen book.</p>
     )
   }
 }
