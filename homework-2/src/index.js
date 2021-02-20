9715dc1d5c2deefcac5b0fda683154c1acc35ce0
Hennady95 <gena.trusevich@mail.ru>
2021-02-20 12:19:16 +0300

    feat: add classwork

--- homework-2/src/index.js
+++ homework-2/src/index.js
@@ -4,9 +4,49 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Textcomp = (props) => {
+  let under = '';
+  if(props.underline) {
+  under = 'underline'
+  } else {
+  under = 'none'
+  }
+  return <span style = {{fontSize: props.size, color: props.color, textDecoration: under}}>sdfkgkjdskjfhdgfdgdgdgsddskgk</span>
+  }
+  
+  class Textcomp2 extends React.Component {
+  
+  state = {
+  value: 'X'
+  }
+  
+  change = () => this.state.value === 'X' ? this.setState({value: 'V'}) : this.setState({value: 'X'})
+  
+  render () {
+  return <div onClick = {this.change} style ={{fontSize: '25px', textAlign: 'center'}}>{this.state.value}</div>
+  }
+  }
+  
+  class Textcomp3 extends React.Component {
+  
+  state = {
+  ind: 0,
+  obj: [11,22,33]
+  }
+  
+  changeValue = () => this.state.ind < this.state.obj.length - 1 ? this.setState({ind: this.state.ind + 1}) : this.setState({ind: 0})
+  
+  render () {
+  return <div onClick = {this.changeValue} style = {{fontSize: '25px', textAlign: 'center'}}>{this.state.obj[this.state.ind]}</div>
+  }
+  }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Textcomp size = '25px' color = 'red' underline />
+    <Textcomp2 />
+    <Textcomp3 />
   </React.StrictMode>,
   document.getElementById('root')
 );
