f1c87fecec945acf7db8c5b0e81af1c9021b8bbf
Hennady95 <gena.trusevich@mail.ru>
2021-02-20 11:49:12 +0300

    feat: complete first task

--- homework-2/src/App.js
+++ homework-2/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import React from 'react';
+import './index.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className = 'App'>
+        <Square label ='Big squere' cssClass = 'square-big' />
+        <Square label ='A less squere' cssClass = 'square-less' />
+        <Square label ='The smallest one' cssClass = 'square-smollest' />
+      </div>
+    )
+  }
+}
+
+class Square extends React.Component {
+  render () {
+    return (
+      <div className = {this.props.cssClass}>
+        {this.props.label}
+      </div>
+    )
+  }
 }
 
 export default App;
