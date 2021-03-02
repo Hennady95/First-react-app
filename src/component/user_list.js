fef48ca7e4ad74c0c50c54d630731f1377ff9396
Hennady95 <gena.trusevich@mail.ru>
2021-03-02 20:36:04 +0300

    refactor: add classes for list components

--- src/component/user_list.js
+++ src/component/user_list.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import data from '../data.json' 
+import '../App.css'
 
 const Usercomponent = (props) => {
     return (
-        <div style = {{border: '2px solid black', padding: '10px', textAlign: 'center', margin: '5px', width: '300px' }}>
+        <div className = 'list-component'>
             <h3>{props.name}</h3>
             <h4>{props.surname}</h4>
         </div>
@@ -13,7 +14,7 @@ const Usercomponent = (props) => {
 class Userlist extends React.Component {
     render() {
         console.log(data);
-        return <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}> {data.map(item => <Usercomponent key={item._id} name = {item.name.first} surname = {item.name.last}/> ) } </div>
+        return <div className = 'User-list-body '> {data.map(item => <Usercomponent key={item._id} name = {item.name.first} surname = {item.name.last}/> ) } </div>
     }
 }
 
