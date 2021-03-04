23f096618ee36f31394b23bd0191b5f9b4bacc9c
Hennady95 <gena.trusevich@mail.ru>
2021-03-04 19:51:57 +0300

    complete first task

---
+++ src/components/conditional-render/index.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import '../conditional-render/style.css'
+
+class Loader extends Component {
+    
+    state = {
+        loading: false,
+        greeting: ''
+    }
+
+    click = () => {
+        setTimeout(this.sayhi,3000)
+        this.setState({loading: true})
+    }
+
+    sayhi = () => this.setState({loading: false, greeting: this.state.greeting === '' ? 'Hello' : ''})
+
+    render() 
+    {
+        return <div className = 'loader-body'>
+            <button className = 'start-loader' onClick = {this.click}>Say HI</button>
+            <div>
+                {this.state.loading ?  <div className = 'loader'></div> : <span className = 'hello-message'>{this.state.greeting}</span>}
+            </div>
+        </div>
+    }
+}
+
+export default Loader;
