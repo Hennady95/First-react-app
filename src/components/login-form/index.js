a1a88c8966f24f72f6fa350561ac89d30b8322fc
Hennady95 <gena.trusevich@mail.ru>
2021-03-06 17:41:44 +0300

    add: new function change state values

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -12,14 +12,16 @@ class Form extends Component {
 
     onChangePassword = (event) => this.setState({password: event.target.value})
 
+    changeInputValue = (event) => event.target.id === 'login' ?  this.setState({login: event.target.value}) : this.setState({password: event.target.value})
+
     clearData = () => this.setState({login: '', password: ''})
 
     render() {
         return <div className = "form">
             <p className = "field-title">Login</p>
-            <input type = "text" onChange = {this.onChangeLogin} value = {this.state.login}/>
+            <input type = "text" onChange = {this.changeInputValue} value = {this.state.login} id='login'/>
             <p className = "field-title">Password</p>
-            <input type = "text" onChange = {this.onChangePassword} value = {this.state.password}/>
+            <input type = "text" onChange = {this.changeInputValue} value = {this.state.password} id='password'/>
             <button className = "clearBtn" onClick = {this.clearData}>Clear data</button>
         </div>
     }
