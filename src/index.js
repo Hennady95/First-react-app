67317cb5b701bc90bfd8a879ec4301a4b7c2b458
Hennady95 <55188385+Hennady95@users.noreply.github.com>
2021-04-02 20:53:03 -0700

    add payload from action

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css'
 
-import { createStore } from 'redux'
-import { reducer } from './reducer.js'
-import { Provider, useSelector, useDispatch } from 'react-redux'
+import { createStore } from 'redux'// для создания общего стейта
+import { reducer } from './reducer.js'//файл логики работы коунтера
+import { Provider, useSelector, useDispatch } from 'react-redux'//обязательные вещи в самом начале
+
+//украшения для задания
+import { Switch, FormControlLabel, Button } from '@material-ui/core';
 
 const actionPlus = {
   type: "PLUS"
@@ -18,24 +22,72 @@ const actionReset = {
   type: "RESET"
 }
 
+const actionTenUm = {
+  type: "UM10" 
+}
+
+const actionChangeTheme = {
+  type: "CHANGE_THEME"
+}
+
 const App = () => {
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch();//вызов логики действия из файла reducer.js (точнее диспетчера событий)
 
-  const number = useSelector((state) => state.number);
+  const number = useSelector((state) => state.number);//данные из стора получаем useSelector
+
+  const theme = useSelector((state) => state.checkValue);
+
+  const buttonTheme = useSelector((state) => state.theme);
+
+  const onChangeTheme = useCallback((event) => {
+      const switchTheme ={
+        type: "SWITCH_THEME_ACTION",
+        payload: event.target.checked//для передачи данных вместе с действием
+      }
+      dispatch(switchTheme)
+  }, [dispatch])
+
+  const onChooseLightTheme = useCallback((event) => {
+    const themeAction = {
+      type: "SWITCH_THEME",
+      payload: event.target.id
+    }
+
+    dispatch(themeAction)
+  },[dispatch]);
 
   return (
-    <div className = "App">
-      <button onClick = {() => dispatch(actionPlus)}>+</button>
-      <p>{number}</p>
-      <button onClick = {() => dispatch(actionMinus)}>-</button>
-      <button onClick = {() => dispatch(actionReset)}>reset</button>
+    <div >
+      <div className = {`App ${buttonTheme}`}>
+        <button onClick = {() => dispatch(actionPlus)}>+</button>
+        <p>{number}</p>
+        <button onClick = {() => dispatch(actionMinus)}>-</button>
+        <button onClick = {() => dispatch(actionReset)}>reset</button>
+        <button onClick = {() => dispatch(actionTenUm)}>X10</button>
+      </div>
+      <div className = {`App ${theme ? "darkTheme" : "lightTheme"}  `}>
+        <p>задание с переключением темы компонента</p>
+        <FormControlLabel
+          control={<Switch size="small" color = "primary" onChange = {()=> dispatch(actionChangeTheme)}/>}
+          label="dark theme"
+        />
+        <FormControlLabel
+          control={<Switch size="small" color = "primary" onChange = {onChangeTheme}/>}
+          label="dark theme"
+        />
+        <button onClick = {onChooseLightTheme} id = 'light' color="primary"> Light</button>
+        <button onClick = {onChooseLightTheme} id = 'dark'> Dark</button>
+        <button onClick = {onChooseLightTheme} id = 'orange'> Orange</button>
+      </div>
     </div>
   )
 
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer); //создание стейта  и передача функции работающей/изменяющей с ним в качестве параметра
+
+//Provider для передачи созданного стейта(store) для компонентов внутри него
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducer.js
+++ src/reducer.js
@@ -1,24 +1,56 @@
-const initialState = {
+const initialState = {//начальный стейт
     number: 0,
+    checkValue: false,
+    theme: 'dark'
 }
 
 export const reducer = (state = initialState, action) => {
-
     if(action.type === "PLUS") {
         return {
+            ...state,
             number: state.number + 1
         }
     }
 
     if(action.type === "MINUS") {
         return {
+            ...state,
             number: state.number - 1
         }
     }
 
     if(action.type === "RESET") {
         return {
+            ...state,
             number: 0
+        }
+    }
+
+    if(action.type === "UM10") {
+        return {
+            ...state,
+            number: state.number * 10
+        }
+    }
+
+    if(action.type === "CHANGE_THEME") {
+        return {
+            ...state,
+            checkValue: !state.checkValue
+        }
+    }
+
+    if(action.type === "SWITCH_THEME_ACTION") {
+        return {
+            ...state,
+            checkValue: action.payload
+        }
+    }
+
+    if(action.type === "SWITCH_THEME") {
+        return {
+            ...state,
+            theme: action.payload
         }
     }
 
